2944bd763045589a246b8002099162c7ca234d86
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-13 12:57:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+class Dog 
+  attr_accessor :name
+  
+  @@all = []
+  @@dog_names= []
+  
+  def initialize (name)
+    @@all<< self  
+    @@dog_names<< name 
+  end 
+  
+  def name
+    @name = name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+      @@all.clear
+  end 
+  
+  def self.print_all
+     @@all.map do |name|
+      puts name
+     end
+  end
+  
+end 
